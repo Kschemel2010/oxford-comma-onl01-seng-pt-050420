c8900f52ce8e8053d352b1738fc28b109a03ca9c
kschemel2010 <kschemel2010@gmail.com>
2020-05-11 23:35:34 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+   if array.length == 1 
+    return "#{array[0]}"
+    elsif array.length ==2 
+    return array.join (' and ')
+    else array.length >= 3
+    push = "and #{array[-1]}"
+    array.pop
+    array.push(push)
+    return array.join(", ")
+   end 
 end
